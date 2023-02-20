55185a7c1e98fc23cda1aaf1190e20ca46eebe81
wxz <759126349@qq.com>
2023-02-20 14:22:25 +0800

    优化代码

--- src/main/java/com/example/mybatis_plus/mapper/UserMapper.java
+++ src/main/java/com/example/mybatis_plus/mapper/UserMapper.java
@@ -10,4 +10,13 @@ import com.example.mybatis_plus.domain.User;
  */
 
 public interface UserMapper extends BaseMapper<User> {
+
+    /**
+     * 查询用户
+     * @author wxz
+     * @date 14:18 2023/2/20
+     * @param id 用户ID
+     * @return com.example.mybatis_plus.domain.User
+     */
+    User findUser(Long id);
 }

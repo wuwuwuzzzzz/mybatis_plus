468b4ece8ee5263a4378a3de921c2a0585f0754e
wxz <759126349@qq.com>
2023-02-20 14:31:35 +0800

    优化代码

--- src/main/java/com/example/mybatis_plus/mapper/UserMapper.java
+++ src/main/java/com/example/mybatis_plus/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package com.example.mybatis_plus.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.example.mybatis_plus.domain.User;
+import org.apache.ibatis.annotations.Param;
 
 /**
- *
  * @author wxz
  * @date 21:28 2023/2/19
  */
@@ -13,10 +15,21 @@ public interface UserMapper extends BaseMapper<User> {
 
     /**
      * 查询用户
-     * @author wxz
-     * @date 14:18 2023/2/20
+     *
      * @param id 用户ID
      * @return com.example.mybatis_plus.domain.User
+     * @author wxz
+     * @date 14:18 2023/2/20
      */
     User findUser(Long id);
+
+    /**
+     * 查询用户
+     *
+     * @param wrapper wrapper
+     * @return com.example.mybatis_plus.domain.User
+     * @author wxz
+     * @date 14:28 2023/2/20
+     */
+    User findUserByWrapper(@Param(Constants.WRAPPER) Wrapper<User> wrapper);
 }
